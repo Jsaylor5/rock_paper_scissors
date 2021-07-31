9bf69fd76db13cd0fcefa672b163830868aa09f5
Jsaylor5 <jsaylor5@kent.edu>
2021-07-31 19:43:37 -0400

    Finished game code logic. Added scoring system

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ rock = choices[0]
 paper = choices[1]
 scissors = choices[2]
 
-computerScore = []
-playerScore = []
+playerScore = 0
+computerScore = 0
+
 
 function computerPlay(){
    return choices[Math.floor(Math.random() * choices.length)];
@@ -19,27 +20,35 @@ function playRound(playerSelection, computerSelection){
         return "tie";
     }
     else if (playerSelection === rock && computerSelection === paper){
+        computerScore++
         return " Computer wins, paper beats rock";
     }
     else if (playerSelection === rock && computerSelection === scissors){
+        playerScore++
         return "Player wins, rock beats scissors";
     }
     else if (playerSelection === paper && computerSelection === paper){
         return "tie";
     }
     else if (playerSelection === paper && computerSelection === scissors){
+        computerScore++
         return "Computer wins, scissors cut paper";
+        
     }
     else if (playerSelection === paper && computerSelection === rock){
+        playerScore++
         return "Player wins, paper beats rock";
+        
     }
     else if (playerSelection === scissors && computerSelection === scissors){
         return "tie";
     }
     else if (playerSelection === scissors && computerSelection === rock){
+        computerScore++
         return "Computer wins, rock beats scissors";
     }
     else if (playerSelection === scissors && computerSelection === paper){
+        playerScore++
         return "Player wins, scissors cut paper";
     }
 
@@ -51,4 +60,16 @@ function game(){
     const computerSelection = computerPlay();
     console.log(playRound(playerSelection, computerSelection));
 }
+score = "player score: " + playerScore + " " + "computer score: " + computerScore
+alert(score)
+if (playerScore > computerScore){
+    return "You win";
+}
+else if (playerScore == computerScore){
+    return "you tied"
+}
+else {
+    return "you lose";
+}
+
 }
